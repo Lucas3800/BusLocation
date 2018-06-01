54ee5008661e38bfec4d7050bf0f93a5ce7a3822
Lucas Almeida <lucas3800@live.com>
2018-06-01 18:16:47 -0300

    importando biblioteca FirebaseAuth

--- app/src/main/java/br/com/buslocation/projeti/buslocation/LoginActivity.java
+++ app/src/main/java/br/com/buslocation/projeti/buslocation/LoginActivity.java
@@ -3,7 +3,11 @@ package br.com.buslocation.projeti.buslocation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
