8f04c8bff8f63bfc18edd5b422057d729c6ddad7
Lucas Almeida <lucas3800@live.com>
2018-06-03 03:15:17 -0300

    v3 Pré-Finalizado CadastroActivity, inicio menuActivity

--- app/src/main/java/br/com/buslocation/projeti/buslocation/MainActivity.java
+++ app/src/main/java/br/com/buslocation/projeti/buslocation/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.buslocation.projeti.buslocation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,15 +12,19 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private android.support.v7.widget.Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent startIntet = new Intent(MainActivity.this, StartActivity.class);
-        startActivity(startIntet);
+        //Firebase
         mAuth = FirebaseAuth.getInstance();
+        //Toolbar
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Main");
     }
 
     public void onStart() {
@@ -26,9 +32,30 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if (currentUser == null) {
-            Intent startIntet = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(startIntet);
-            finish();
+            sendToStart();
         }
+    }
+
+    private void sendToStart() {
+        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_menu_sair)
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+        return true;
     }
 }
